f6f40c1ae69f7f891988a359ea81f03eddff1b5f
Nadiar Syaripul <codxse@gmail.com>
2020-12-19 09:29:54 +0700

    guess

--- glasgow-university/src/Lib.hs
+++ glasgow-university/src/Lib.hs
@@ -18,3 +18,17 @@ listOfString :: [[Char]]
 listOfString = ["a", "list", "of", "Strings"]
 
 anonimFunc = \x -> x * x
+
+roots a b c = 
+  let 
+    det2 = b*b-4*a*c
+    det = sqrt det2
+    rootp = (-b + det)/a/2
+    rootm = (-b - det)/a/a
+  in
+    [rootm, rootp]
+    
+max2 a c = if a > c then a else c
+   
+    
+  
